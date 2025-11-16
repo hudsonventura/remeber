120820ecfd7ebbf103237b2376bbae78af1afbb0
Hudson Ventura <hudsonventura@outlook.com>
2025-11-15 21:41:18 -0400

    Adding migrations

---
+++ src/server/Migrations/20251115164046_AddActiveToBackupPlan.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddActiveToBackupPlan : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "active",
+                table: "backup_plan",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "active",
+                table: "backup_plan");
+        }
+    }
+}
